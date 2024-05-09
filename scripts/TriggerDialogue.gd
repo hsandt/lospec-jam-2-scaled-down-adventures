357b39fc364b7b33d18ccff14c547556fe980167
hsandt <1067772+hsandt@users.noreply.github.com>
2024-05-09 16:56:46 +0200

    [Input] Add gamepad nav input, TriggerDialogue checks dialogic_default_action

--- scripts/TriggerDialogue.gd
+++ scripts/TriggerDialogue.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 
-func _input(event: InputEvent):
+func _unhandled_input(event: InputEvent):
 	# check if a dialog is already running
 	if Dialogic.current_timeline != null:
 		return
 
-	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
+	if event.is_action_pressed(&"dialogic_default_action"):
 		Dialogic.start('timeline')
 		get_viewport().set_input_as_handled()
