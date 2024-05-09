0d5a340185526aa71ecf7277b43b31d020c2552f
hsandt <1067772+hsandt@users.noreply.github.com>
2024-05-09 15:01:15 +0200

    [PlayerCharacter] Code cardinal motion, add move cardinal dir inputs (keyboard arrows), PC layer collides with Wall layer
    - rename test_yarn_spinner.tscn -> test_top_view.tscn
    - import white_square_1x1.png
    - demonstrate with PC and wall squares

---
+++ scripts/PlayerCharacter/PlayerCharacter.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+## Move speed applied to each cardinal axis (moving diagonally is sqrt(2) times faster)
+@export var move_cardinal_speed: float = 100.0
+
+## Move intention vector: ternary value (-1, 0, +1) for each cardinal axis
+var move_intention: Vector2
+
+
+func _process(delta: float):
+	move_intention = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
+
+
+func _physics_process(delta: float):
+	velocity = move_cardinal_speed * move_intention
+	move_and_slide()
