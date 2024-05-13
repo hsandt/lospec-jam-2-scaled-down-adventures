a69e724ccfb7386ed0a18517f97cde1f649f72c9
hsandt <1067772+hsandt@users.noreply.github.com>
2024-05-13 22:14:57 +0200

    [Room] Allow F6 play from any Room scene using new GameManager autoload singleton to load additive InGame scene if needed
    and InGameManager checks that there is not already a scene before loading first scene
    
    The previous commit was important to allow Maaack's Menus Template Play button to properly load the InGame scene, despite being already assigned to GameManager and therefore cached.

---
+++ scripts/GameManager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+@export var in_game_scene: PackedScene
+
+@onready var in_game_manager: InGameManager = get_tree().get_first_node_in_group("in_game_manager")
+@onready var room_in_played_scene: Room = get_tree().get_first_node_in_group("rooms")
+
+
+func _ready():
+	DebugUtils.assert_member_is_set(self, in_game_scene, "in_game_scene")
+
+	# Check if we are directly F6 playing a Room scene instead of playing
+	# the InGame scene (identified by the InGameManager)
+	if room_in_played_scene and not in_game_manager:
+		# To make it easy to debug game from any Room scene,
+		# dynamically load the InGame scene now (it will detect the existing
+		# room on its own ready and therefore not try to load the first scene on top)
+		NodeUtils.instantiate_under.call_deferred(in_game_scene, get_tree().root)
--- scripts/InGameManager.gd
+++ scripts/InGameManager.gd
@@ -8,6 +8,7 @@ extends Node
 ## Index of warp entrance spot in first room to start at
 @export var first_warp_entrance_spot_index: int
 
+@onready var room_on_ready: Room = get_tree().get_first_node_in_group("rooms")
 @onready var camera: InGameCamera = $%InGameCamera
 @onready var player_character: PlayerCharacter = get_tree().get_first_node_in_group("player_character")
 
@@ -17,7 +18,11 @@ var current_room_instance: Room
 func _ready():
 	DebugUtils.assert_member_is_set(self, first_room_scene, "first_room_scene")
 
-	load_room_scene_deferred(first_room_scene, first_warp_entrance_spot_index)
+	# In normal play, there is no other room as we load InGame scene alone
+	# In debug F6 play a specific room, we load InGame scene additively from
+	# GameManager, and so we must not also load the first scene on top
+	if not room_on_ready:
+		load_room_scene_deferred(first_room_scene, first_warp_entrance_spot_index)
 
 
 ## Load room scene deferred, so it works during initialization and collision checks
@@ -28,7 +33,7 @@ func load_room_scene_deferred(room_scene: PackedScene, warp_entrance_spot_index:
 ## Load passed room scene and warp PC to entrance spot of index warp_entrance_spot_index
 func _load_room_scene(room_scene: PackedScene, warp_entrance_spot_index: int):
 	# instantiate room scene below same parent (InGame root node)
-	current_room_instance = NodeUtils.instantiate_under(room_scene, get_parent())
+	current_room_instance = NodeUtils.instantiate_under(room_scene, get_tree().root)
 	camera.set_camera_limits_for_room(current_room_instance)
 
 	if warp_entrance_spot_index < current_room_instance.entrance_spots.size():
