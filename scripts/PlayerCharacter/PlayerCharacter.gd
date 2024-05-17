41228a35ba086bd9e38bf2c24e2500ac26fbdfc3
hsandt <1067772+hsandt@users.noreply.github.com>
2024-05-17 15:03:13 +0200

    [PC] Rotate new DirectionalParent containing InteractableArea to match last movement direction

--- scripts/PlayerCharacter/PlayerCharacter.gd
+++ scripts/PlayerCharacter/PlayerCharacter.gd
@@ -2,11 +2,16 @@ class_name PlayerCharacter
 extends CharacterBody2D
 
 
+## Parent of all objects that should rotate by multiple of 90 degrees when character changes
+## direction (generally Area2D nodes)
+@export var directional_parent: Node2D
+
 ## Area to detect interactable in
 @export var interaction_area: Area2D
 
 ## Move speed applied to each cardinal axis (moving diagonally is sqrt(2) times faster)
 @export var move_cardinal_speed: float = 100.0
+
 
 ## Move intention vector: ternary value (-1, 0, +1) for each cardinal axis
 var move_intention: Vector2
@@ -16,6 +21,7 @@ var is_interacting: bool
 
 
 func _ready():
+	DebugUtils.assert_member_is_set(self, directional_parent, "directional_parent")
 	DebugUtils.assert_member_is_set(self, interaction_area, "interaction_area")
 
 
@@ -24,6 +30,9 @@ func _process(_delta: float):
 
 
 func _physics_process(_delta: float):
+	if move_intention != Vector2.ZERO:
+		_update_direction_toward(move_intention)
+
 	velocity = move_cardinal_speed * move_intention
 	move_and_slide()
 
@@ -36,6 +45,13 @@ func _unhandled_input(event: InputEvent):
 	if event.is_action_pressed(&"dialogic_default_action"):
 		_process_interact()
 		get_viewport().set_input_as_handled()
+
+
+func _update_direction_toward(move_intention: Vector2):
+	# DirectionalParent children must all face RIGHT in initial scene setup, as it is
+	# Godot's angle reference
+	var cardinal_direction := MathUtils.vector2i_to_dominant_cardinal_direction(move_intention, true)
+	directional_parent.rotation = MathUtils.cardinal_direction_to_angle(cardinal_direction)
 
 
 func can_interact() -> bool:
@@ -69,6 +85,7 @@ func _stop_interaction():
 # Interact
 func _interact(interactable_area: InteractableArea):
 	# TODO: get dialogue from interactable
+	# TODO: preload to avoid lag on first dialogue
 	var layout := Dialogic.start('timeline')
 	layout.register_character(interactable_area.dialogic_character, interactable_area.owning_entity)
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
